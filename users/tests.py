32cfc29f71b5bcf090f02d2fe1ce57b2dc04bc5b
ducoopa <nguoihanhtinh123@gmail.com>
2020-06-18 00:58:10 +0700

    test profile view

--- users/tests.py
+++ users/tests.py
@@ -5,16 +5,24 @@ from django.test import Client
 # Create your tests here.
 
 class ProfileTestCase(TestCase):
+    id_1 = {
+        'username' : 'duc_v10',
+        'password' : 'duc_dep_trai',
+    }
     def setUp(self):
-        User.objects.create(username='duc_v10', password='duc_dep_trai')
-        User.objects.create(username='duc_v20', password='duc_soai_ca')
+        duc_v10 = User.objects.create_user(**self.id_1)
     
-    def test_profile(self):
-        """Username of Profiles must have the same name with Username of User"""
-        profile_v10 = User.objects.get(username='duc_v10').profile
-        profile_v20 = User.objects.get(username='duc_v20').profile
-        self.assertEqual(profile_v10.__str__(), 'Profile: duc_v10')
-        self.assertEqual(profile_v20.__str__(), 'Profile: duc_v20')
+    def test_exist_profile(self):
+        profile_v10 = User.objects.get(username=self.id_1['username']).profile
+        self.assertEqual(profile_v10.__str__(), f"Profile: {self.id_1['username']}")
+
+    def test_profile_view(self):
+        client = Client()
+        logged_in = client.login(**self.id_1)
+        if logged_in:
+            id_user = client.session['_auth_user_id']
+            response = client.get(f"/profile/{id_user}/")
+            self.assertEqual(response.status_code, 200)
 
 class LoginTest(TestCase):
     def setUp(self):
@@ -27,3 +35,5 @@ class LoginTest(TestCase):
     def test_login(self):
         client = Client()
         logged_in = client.login(**self.anonymous)
+
+    
